b682c3a7828d8fe3ce92899fdd522ffea9c5705e
sushantssn <sushant.nalawade@yahoo.in>
2022-09-11 10:14:36 +0530

    Initial Commit of Rating MicroService

---
+++ src/main/java/com/ratings/dataservice/ratings/dataservice/Application.java
@@ -0,0 +1,13 @@
+package com.ratings.dataservice.ratings.dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratings/dataservice/ratings/dataservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratings.dataservice.ratings.dataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
