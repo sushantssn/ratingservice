19f377bd5e053840c3809d70364fe4c8ab14c1ed
Sushant <sushant.nalawade@yahoo.in>
2022-09-27 00:30:17 +0530

    Enabling Eureka Client

--- src/main/java/com/ratings/dataservice/ratings/dataservice/controller/RatingResourceController.java
+++ src/main/java/com/ratings/dataservice/ratings/dataservice/controller/RatingResourceController.java
@@ -1,6 +1,7 @@
 package com.ratings.dataservice.ratings.dataservice.controller;
 
 import com.ratings.dataservice.ratings.dataservice.pojo.Rating;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingResourceController {
-@RequestMapping("/{movieId")
+@GetMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
 
     return new Rating(movieId,4);
